bbb17257eeb7a77769a632fb31c77f9f6da02d62
Vikas <vikasr1989@gmail.com>
2014-07-03 13:22:02 +0530

    Added int compare function and modified quicksort to take function pointer for comparision

--- quicksort.c
+++ quicksort.c
@@ -1,9 +1,12 @@
-/************************************************
+/******************************************************************************
  * FILE NAME - quicksort.c
  *
  * REVISION  DATE      AUTHOR          CHANGES
  *    1      03/07/14  Vikas R         Added quicksort routine and test code
- * *********************************************/
+ *    2      03/07/14  Vikas R         Added int compare function and modified
+ *                                     quicksort to take function pointer
+ *                                     for comparision
+ * ***************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -17,30 +20,32 @@
     }                          \
     printf("\n");              \
     
-void swap(int v[],int i, int j);
-void my_qsort(int v[],int left,int right);
+void my_swap(int v[],int i, int j);
+void my_qsort(int v[],int left,int right, int (*comp)(void*,void*));
+int  my_int_compare_function(int *a,int *b);
 
-/************************************************
+/******************************************************************************
  * FUNCTION NAME - main
  * DESCRIPTION   - This is the test function to test quicksort routine
  * ARGUMENTS     - None
  * RETURN        - SUCCESS(0) / FAILURE (NON-ZERO)
- * *********************************************/
-
+ * ***************************************************************************/
 int main()
 {
     int i;
     int a[] = {98,76,1,43,21};
+    int (*comp)(void*,void*);
     printf("before sorting\n");
     PRINT_ELEMENTS(a,RIGHT+1);
     
-    my_qsort(a,LEFT,RIGHT);
+    comp = (int (*)(void *,void *))my_int_compare_function;
+    my_qsort(a,LEFT,RIGHT,comp);
     printf("after sorting\n");
     PRINT_ELEMENTS(a,RIGHT+1);
     return 0;
 }
 
-/************************************************
+/******************************************************************************
  * FUNCTION NAME - my_qsort
  * DESCRIPTION   - This is the quicksort routine. It is a recursive function 
  *                 which sorts the integer elements provided in the input 
@@ -49,8 +54,8 @@ int main()
  *                 int left - the left index of the array
  *                 int right - the right index of the array
  * RETURN        - NONE
- * *********************************************/
-void my_qsort(int v[],int left,int right)
+ * ***************************************************************************/
+void my_qsort(int v[],int left,int right, int (*comp)(void*,void*))
 {
     int i,last;
     if(left>=right)
@@ -58,29 +63,51 @@ void my_qsort(int v[],int left,int right)
         return;
     }
     last = left;
-    swap(v,left,(left+right)/2);
+    my_swap(v,left,(left+right)/2);
     for(i=left+1;i<=right;i++)
     {
-        if(v[i] < v[left])
+        /* comp has to be a funtion which returns -1 if &v[i]<&v[left] */
+        if((*comp)(&v[i],&v[left]) == -1)
         {
-            swap(v,++last,i);
+            my_swap(v,++last,i);
         }
     }
-    swap(v,left,last);
-    my_qsort(v,left,last-1);
-    my_qsort(v,last+1,right);
+    my_swap(v,left,last);
+    my_qsort(v,left,last-1,comp);
+    my_qsort(v,last+1,right,comp);
 }
-/************************************************
- * FUNCTION NAME - swap
+/******************************************************************************
+ * FUNCTION NAME - my_swap
  * DESCRIPTION   - Swaps 2 elements of the array v of indices i and j
  * ARGUMENTS     - int v[] - pointer to the array of elements
  *                 int i   - Element to be swapped
  *                 int j   - Element to be wapped with
  * RETURN        - NONE
- * *********************************************/
-void swap(int v[],int i, int j)
+ * ***************************************************************************/
+void my_swap(int v[],int i, int j)
 {
     int temp = v[i];
     v[i] = v[j];
     v[j] = temp;
+}
+/******************************************************************************
+ * FUNCTION NAME - my_int_compare_function
+ * DESCRIPTION   - Compares 2 integers
+ * ARGUMENTS     - int *a   - Address of first integer to be compared
+ *                 int *b   - Address of second integer to be compared
+ * RETURN        - -1 if *a < *b
+ *                 1  if *a > *b
+ *                 0  if *a = *b
+ * ***************************************************************************/
+int my_int_compare_function(int *a,int *b)
+{
+    int iRet = 0;
+    if(*a < *b)
+        iRet = -1;
+    if(*a > *b)
+        iRet = 1;
+    if(*a == *b)
+        iRet = 0;
+
+    return iRet;
 }
